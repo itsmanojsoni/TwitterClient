cc9707fe27e96b57e3dfee40f7640b1a7ca60d94
manoj <manoj@flipagram.com>
2017-10-05 05:32:50 -0700

    Working fine

--- twitterClient/TwitterClient/app/src/main/java/com/codepath/apps/twitterclient/fragment/MentionFragment.java
+++ twitterClient/TwitterClient/app/src/main/java/com/codepath/apps/twitterclient/fragment/MentionFragment.java
@@ -18,7 +18,7 @@ import com.codepath.apps.twitterclient.R;
  * Use the {@link MentionFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MentionFragment extends Fragment {
+public class MentionFragment extends TimeLineFragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
